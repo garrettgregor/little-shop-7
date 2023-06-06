0eaf938489176bfa9639e2ba8746a4b17846d267
Jimmy Jurado <jurado140@gmail.com>
2023-06-05 22:53:24 -0400

    Updates controller, adds edit view, and update routes. Test pass, missing current path assertion and routes cleanup.

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -11,14 +11,20 @@ class Merchants::ItemsController < ApplicationController
 
   def edit
     @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
   end
 
   def update
     @merchant = Merchant.find(params[:merchant_id])
-    @merchant.update(merchant_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
 
-    redirect_to merchant_item_path
-    
+    redirect_to merchant_item_path    
   end
 
+  private
+
+  def item_params
+    params.permit(:name, :description, :status, :unit_price)
+  end
 end
