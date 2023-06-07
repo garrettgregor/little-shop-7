97a81e7b0632da95b1ac3f6ab67f1bc362ea111c
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-07 15:34:36 -0600

    US37: Feat: shop image

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,10 @@
+class UnsplashService
+  def logo_url
+    get_url("https://api.unsplash.com/photos/MRjjcDIk3Gw?client_id=Un0YxxAZVKw0udaKi8OZgX4PkfMxqU9wDSvvADb4n88")
+  end
+
+  def get_url(url)
+    response = HTTParty.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
