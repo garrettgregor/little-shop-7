164ebb2f2bfdf22d6999af13024579b5590e481e
Julian Beldotti <julian.beldotti@gmail.com>
2023-06-07 18:53:02 -0500

    API: Add likes to all pages for logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   helper_method :logo
+  helper_method :show_logo_likes
 
   def logo
     @logo_thumb = LogoSearch.new.logo_image.logo_thumb_url
+  end
+
+  def show_logo_likes
+    @number_likes = LogoSearch.new.logo_image.logo_likes
   end
 
   def welcome
