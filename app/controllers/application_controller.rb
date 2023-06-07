788293606fbc04a7ae4902514f609583ba43f3c4
Austin Woods <119133908+boomclear@users.noreply.github.com>
2023-06-07 17:53:53 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   def welcome
   end
-
-  ## For later error messages...
-  # private
-
-  #   def error_message(errors)
-  #     errors.full_messages.join(', ')
-  #   end
 end
