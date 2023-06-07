117ac062d4efcaab92ed43b574b46a18beb19dbc
Jimmy Jurado <jurado140@gmail.com>
2023-06-07 14:25:38 -0400

    Finishes the final user story and adds model testing. Update item index view with css and updates item show page with css styling.

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,13 @@ class Item < ApplicationRecord
   enum :status, [:disabled, :enabled]
 
   def top_selling_date
-    Item.joins(invoices: :transactions)
+    invoice = Item.joins(invoices: :transactions)
     .where('transactions.result = ? and invoice_items.item_id = ?', "1", self.id)
     .select("invoices.*, SUM(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
     .group("invoices.id")
     .order(total_revenue: :desc)
     .limit(1)
-    .first.created_at.strftime("%A, %B %e, %Y")
+    .first
+    invoice.created_at.strftime("%A, %B %e, %Y")
   end
 end
